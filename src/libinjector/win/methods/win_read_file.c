25ec10b91c54bed9a6aed2c5b24c8f78e6777730
c7f m0d3 <c7f.m0d3@gmail.com>
2022-05-14 00:44:27 +0000

    Workaround file read timeouts in injector during create_rekall_profile
    TODO: COMMIT MESSAGE
    
    Tested on top of upstream commit 236b3e59d7942e4c91ef867edac12f5fe1a2004a.

--- src/libinjector/win/methods/win_read_file.c
+++ src/libinjector/win/methods/win_read_file.c
@@ -255,19 +255,14 @@ event_response_t handle_readfile_x64(drakvuf_t drakvuf, drakvuf_trap_info_t* inf
             if (is_fun_error(drakvuf, info, "Could not close File handle"))
                 return cleanup(injector, info);
 
-            memcpy(info->regs, &injector->x86_saved_regs, sizeof(x86_registers_t));
-
             PRINT_DEBUG("File operation executed OK\n");
             injector->rc = INJECTOR_SUCCEEDED;
 
-            event = VMI_EVENT_RESPONSE_SET_REGISTERS;
-            break;
-        }
-        case STEP8: // exit loop
-        {
             drakvuf_remove_trap(drakvuf, info->trap, NULL);
             drakvuf_interrupt(drakvuf, SIGDRAKVUFERROR);
-            event = VMI_EVENT_RESPONSE_NONE;
+
+            memcpy(info->regs, &injector->x86_saved_regs, sizeof(x86_registers_t));
+            event = VMI_EVENT_RESPONSE_SET_REGISTERS;
             break;
         }
         default:
@@ -282,9 +277,15 @@ event_response_t handle_readfile_x64(drakvuf_t drakvuf, drakvuf_trap_info_t* inf
 
 static event_response_t cleanup(injector_t injector, drakvuf_trap_info_t* info)
 {
+    drakvuf_t drakvuf = injector->drakvuf;
+
     PRINT_DEBUG("Exiting prematurely\n");
+
+    drakvuf_remove_trap(drakvuf, info->trap, NULL);
+    drakvuf_interrupt(drakvuf, SIGDRAKVUFERROR);
+
     memcpy(info->regs, &injector->x86_saved_regs, sizeof(x86_registers_t));
-    return override_step(injector, STEP8, VMI_EVENT_RESPONSE_SET_REGISTERS);
+    return VMI_EVENT_RESPONSE_SET_REGISTERS;
 }
 
 static bool process_read_file(drakvuf_t drakvuf, drakvuf_trap_info_t* info, uint32_t* num_bytes)
